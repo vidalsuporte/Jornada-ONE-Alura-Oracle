ba0a8a28eaef72ca05ac4215a937a897a3a89b7e
vidalsuporte <vidalsuporte@gmail.com>
2024-06-23 08:38:05 -0300

    Update app.js
    projeto finalizado do curso de logica de programação  em javaScript

--- Iniciante em Programação T7 - ONE/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Iniciante em Programação T7 - ONE/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,21 +1,28 @@
 alert('Bem vindo ao jogo no número secreto!');
-let numeroSecreto = Math.random * 10;
-let chute = prompt('Escolha um número ente 1 e 10. ');
+let numeroLimite = 100;
+let numeroSecreto = parseInt(Math.random() * numeroLimite + 1);
+console.log(numeroSecreto);
+let chute ;
 let tentativas = 1;
 
 
-console.log(numeroSecreto);
-if(chute == numeroSecreto){
+while(chute != numeroSecreto){
 
-    alert(`Você acertou o numero secreto com ${tentativas} ${tentativas > 1? 'tentativas':'tentativa'}!`);    
-}else{
-if(chute > numeroSecreto){
+chute = prompt(`Esolha um numero de 1 a ${numeroLimite}.`)
+    if(chute == numeroSecreto){
+        break;
+    }else{
+        if(chute > numeroSecreto){
 
-    alert('o chute é maior que o numero secreto');
-}else{
+            alert('o chute é maior que o numero secreto.');
+        }else{
 
-    alert('o chute é menor que o numero secreto');
-}
+            alert('o chute é menor que o numero secreto.');
+        }
 tentativas ++;
 
 }
+
+}
+
+alert(`Você acertou o numero secreto com ${tentativas} ${tentativas > 1? 'tentativas':'tentativa'}!`); 
