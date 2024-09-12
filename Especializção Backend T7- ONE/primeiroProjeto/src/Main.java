fa43e183821e26ff80cdd05ae64b73f487f962d4
vidalsuporte <vidalsuporte@gmail.com>
2024-09-12 16:50:25 -0300

    Inicialização do cliente do banco

--- Especializção Backend T7- ONE/primeiroProjeto/src/Main.java
+++ Especializção Backend T7- ONE/primeiroProjeto/src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô mundo!");
+
+        //cliente numero 1
+        String nomeC1 = "André Vidal";
+        String tipoContaC1 = "Conta Corrente";
+        Double saldoC1 = 0.0;
+
+        //cliente numero 2
+        String nomeC2 = "Lucas Gonçalves";
+        String tipoContaC2 = "Conta Corrente";
+        Double saldoC2 = 0.0;
+
+        System.out.println("**********************************");
+        System.out.println("Dados Cliente 1");
+        System.out.println("Nome: " + nomeC1);
+        System.out.println("Tipo de conta: " + tipoContaC1);
+        System.out.println("Saldo: R$"+saldoC1);
+        System.out.println("------------------------------------------");
+        System.out.println("Dados Cliente 2");
+        System.out.println("Nome: " + nomeC2);
+        System.out.println("Tipo de conta: " + tipoContaC2);
+        System.out.println("Saldo: R$"+saldoC1);
+        System.out.println("**********************************");
+
+
+
 
 
 
